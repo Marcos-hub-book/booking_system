20866adca39905b5bd961a73964339cf498300e1
Marcos-hub-book <mistermarcospc@gmail.com>
2025-10-19 08:45:45 -0300

    alteracao user cad

--- app/models.py
+++ app/models.py
@@ -15,10 +15,19 @@ service_professional = db.Table('service_professional',
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
+    full_name = db.Column(db.String(128), nullable=False)
+    company_name = db.Column(db.String(64), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)
-    role = db.Column(db.String(50), nullable=False)  # e.g., 'admin', 'professional'
-    # Não relacione Appointment aqui, pois agora será pelo Customer
+    phone = db.Column(db.String(20))
+    role = db.Column(db.String(50), nullable=False)   # e.g., 'admin', 'professional'
+    cep = db.Column(db.String(10))
+    logradouro = db.Column(db.String(128))
+    numero = db.Column(db.String(10))
+    cidade = db.Column(db.String(64))
+    estado = db.Column(db.String(2))
+    profile_photo = db.Column(db.String(256))  # caminho da foto
+    plan = db.Column(db.String(32), default="free")
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
@@ -86,3 +95,7 @@ class ProfessionalSchedule(db.Model):
     break_end = db.Column(db.Time, nullable=True)
 
     professional = db.relationship('Professional', backref='schedules')
+
+    
+    
+    
